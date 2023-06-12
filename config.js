2fd8484f1e7156f804ab84d902a0f06a4d41ca29
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2023-06-12 13:43:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.github = 'https://github.com/akshitbhatia2004/WHATSAPP_AKBOT'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.sudo = process.env.SUDO || '919971606684'
 global.devs = '919971606684';
-global.website = 'https://github.com/akshitbhatia2004/Secktor-Md' //wa.me/+91000000000000
+global.website = 'https://github.com/akshitbhatia2004/WHATSAPP_AKBOT' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
