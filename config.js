24ad6bdd21a7b2fdf8c5fef9a21c8bd8bb216641
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2023-06-12 13:45:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.devs = '919971606684';
 global.website = 'https://github.com/akshitbhatia2004/WHATSAPP_AKBOT' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+  botname: process.env.BOT_NAME || '𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏_𝐀𝐊𝐁𝐎𝐓',
   ownername:process.env.OWNER_NAME || 'akshitbhatia2004',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
