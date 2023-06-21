16489578da43b97d597069cc07f70fd3be47e445
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2023-06-21 16:13:13 +0530

    Update _pm2.js

--- commands/_pm2.js
+++ commands/_pm2.js
@@ -3,8 +3,8 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @project_name : Whatsapp_Akbot-Md
+ * @author : akshitbhatia2004 <https://github.com/akshitbhatia2004>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
