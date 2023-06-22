2e9be87dc7f142171d762fda07a3e2ababbbd0ed
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2023-06-22 11:50:39 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -6,6 +6,7 @@ cmd({
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
+            const { Insta } = require('../lib')
 if(!text) return citel.reply('Need post url.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
