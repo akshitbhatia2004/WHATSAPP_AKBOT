784b5d6105085b5685afd611cf1d076bf46287b6
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2023-07-29 11:45:26 +0530

    ✅✅

--- commands/converter.js
+++ commands/converter.js
@@ -97,16 +97,16 @@ cmd({
             pattern: "fancy",
             desc: "Makes stylish/fancy given text",
             category: "converter",
-            use: '56 Secktor',
+            use: '56 AkshitBot',
             react: "✅",
             filename: __filename
         },
         async(Void, citel, text) => {
             if (isNaN(text.split(" ")[0]) || !text) {
                 let text = tiny(
-                    "Fancy text generator\n\nExample: .fancy 32 Secktor\n\n"
+                    "Fancy text generator\n\nExample: .fancy 32 AkshitBot\n\n"
                 );
-                listall("Secktor Bot").forEach((txt, num) => {
+                listall("Akshit Bot").forEach((txt, num) => {
                     text += `${(num += 1)} ${txt}\n`;
                 });
                 return await citel.reply(text);
